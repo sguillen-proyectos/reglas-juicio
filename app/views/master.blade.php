21c7dad0a47d1979b1bcc84e5efab4d5ae788ff5
donkeysharp <serguimant@gmail.com>
2014-06-12 10:25:06 -0400

    asd

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
   <title>Sistemas Expertos</title>
   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
